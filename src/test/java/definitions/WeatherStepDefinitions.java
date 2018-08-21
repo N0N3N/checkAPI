e867980e5dce49a84593423c315d0282dfc98348
Romans Dobrijans <rdobrijans@evolutiongaming.com>
2018-08-21 18:05:47 +0300

    Working version

--- src/test/java/definitions/WeatherStepDefinitions.java
+++ src/test/java/definitions/WeatherStepDefinitions.java
@@ -3,7 +3,6 @@ package definitions;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import model.Response;
-import org.junit.Assert;
 
 import static org.junit.Assert.assertEquals;
 
@@ -79,5 +78,73 @@ public class WeatherStepDefinitions {
         assertEquals("Wrong temp_max", temp_max, response.getMain().getTempMax(), 0.001);
     }
 
+    @Then("visibility is (.*)")
+    public void check_visibility(int visibility){
+        assertEquals("Wrong visibility", visibility, response.getVisibility());
+    }
 
+    @Then("speed is (.*)")
+    public void check_speed(double speed){
+        assertEquals("Wrong speed", speed, response.getWind().getSpeed(), 0.001);
+    }
+
+    @Then("deg is (.*)")
+    public void check_deg(int deg){
+        assertEquals("Wrong deg", deg, response.getWind().getDeg());
+    }
+
+    @Then("all is (.*)")
+    public void check_all(int all){
+        assertEquals("Wrong all", all, response.getClouds().getAll());
+    }
+
+    @Then("dt is (.*)")
+    public void check_dt(int dt){
+        assertEquals("Wrong dt", dt, response.getDt());
+    }
+
+    @Then("type is (.*)")
+    public void check_type(int type){
+        assertEquals("Wrong type", type, response.getSys().getType());
+    }
+
+    @Then("sys.id is (.*)")
+    public void check_sys_id(int id){
+        assertEquals("Wrong id", id, response.getSys().getId());
+    }
+
+    @Then("message is (.*)")
+    public void check_message(double message){
+        assertEquals("Wrong message", message, response.getSys().getMessage(), 0.001);
+    }
+
+    @Then("country is (.*)")
+    public void check_country(String country){
+        assertEquals("Wrong country", country, response.getSys().getCountry());
+    }
+
+    @Then("sunrise is (.*)")
+    public void check_sunrise(int sunrise){
+        assertEquals("Wrong sunrise", sunrise, response.getSys().getSunrise());
+    }
+
+    @Then("sunset is (.*)")
+    public void check_sunset(int sunset){
+        assertEquals("Wrong sunset", sunset, response.getSys().getSunset());
+    }
+
+    @Then("id is (.*)")
+    public void check_id(int id){
+        assertEquals("Wrong id", id, response.getId());
+    }
+
+    @Then("name is (.*)")
+    public void check_name(String name){
+        assertEquals("Wrong name", name, response.getName());
+    }
+
+    @Then("cod is (.*)")
+    public void check_cod(int cod){
+        assertEquals("Wrong cod", cod, response.getCod());
+    }
 }
